7b9d4e3768e2d8496b1cb90a5e62aba82d309cdf
Rustam Korsovetskiy <rustamkorsovetskiy2013@gmail.com>
2025-06-25 21:43:41 +0300

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,25 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+    void shouldNewBonus() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1500;
+        int actual = 150;
+        int expected = 150;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldLessAmount() {
+        int amount = 900;
+        int actual = 0;
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+}
